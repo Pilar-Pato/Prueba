fcf43bd74730d045254cf25a547470abfa3ccbe4
lara-gs <lara_989@hotmail.com>
2024-10-10 14:39:53 +0200

    arreglo del modelo para listar incidencias

--- src/main/java/com/factoriaf5/model/IncidenciasModel.java
+++ src/main/java/com/factoriaf5/model/IncidenciasModel.java
@@ -3,88 +3,81 @@ package com.factoriaf5.model;
 import java.util.Date;
 
 public class IncidenciasModel {
-private int idIncidencia;
-private Date fechaCreacion;
-private String descripcion;
-private String titulo;
-private Date fechaCierre;
-private String estado;
 
-public IncidenciasModel(int idIncidencia, Date fechaCreacion, String descripcion, String titulo, Date fechaCierre, String estado){
-    this.idIncidencia = idIncidencia;
-    this.fechaCreacion = fechaCreacion;
-    this.descripcion = descripcion;
-    this.titulo = titulo;
-    this.fechaCierre = fechaCierre;
-    this.estado = estado;
-}
+    private int idIncidencia;
+    private Date fechaCreacion;
+    private String descripcion;
+    private String titulo;
+    private Date fechaCierre;
+    private String estado;
 
-
+    public IncidenciasModel(int idIncidencia, Date fechaCreacion, String descripcion, String titulo, Date fechaCierre, String estado) {
+        this.idIncidencia = idIncidencia;
+        this.fechaCreacion = fechaCreacion;
+        this.descripcion = descripcion;
+        this.titulo = titulo;
+        this.fechaCierre = fechaCierre;
+        this.estado = estado;
+    }
 
 //public IncidenciasModel() {
-  
+    public int getIdIncidencia() {
+        return idIncidencia;
+    }
 
+    public void setIdIncidencia(int idIncidencia) {
+        this.idIncidencia = idIncidencia;
+    }
 
-public int getIdIncidencia(){
-    return idIncidencia;
-}
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
 
-public void setIdIncidencia(int idIncidencia){
-    this.idIncidencia = idIncidencia;
-}
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
 
-public Date getFechaCreacion(){
-    return fechaCreacion;
-}
-public void setFechaCreacion(Date fechaCreacion){
-    this.fechaCreacion = fechaCreacion;
-}
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
 
-public String getDescripcion(){
-    return descripcion;
-}
-public void setDescripcion(String descripcion){
-    this.descripcion = descripcion;
-}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
+    public Date getFechaCierre() {
+        return fechaCierre;
+    }
 
-public String getTitulo(){
-    return titulo;
-}
-public void setTitulo(String titulo){
-    this.titulo = titulo;
-}
+    public void setFechaCierre(Date fechaCierre) {
+        this.fechaCierre = fechaCierre;
+    }
 
+    public String getEstado() {
+        return estado;
+    }
 
-public Date getFechaCierre(){
-    return fechaCierre;
-}
-public void setFechaCierre(Date fechaCierre){
-    this.fechaCierre = fechaCierre;
-}
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
-
-public String getEstado(){
-    return estado;
-}
-public void setEstado(String estado){
-    this.estado = estado;
-}
-
-
-@Override
-public String toString() {
-    return "Incidencias{" +
-            "idIncidencia=" + idIncidencia +
-            ", fechaCreacion='" + fechaCreacion + '\'' +
-            ", descripcion='" + descripcion + '\'' +
-            ", titulo='" + titulo + '\'' +
-            ", fechaCierre='" + fechaCierre + '\'' +
-            ", estado='" + estado + '\'' +
-            '}';
-}
+    @Override
+    public String toString() {
+        return "ID: " + idIncidencia
+                + ", Fecha de Creación: " + (fechaCreacion != null ? fechaCreacion.toString() : "N/A")
+                + ", Descripción: '" + descripcion + '\''
+                + ", Título: '" + titulo + '\''
+                + ", Fecha de Cierre: " + (fechaCierre != null ? fechaCierre.toString() : "N/A")
+                + ", Estado: '" + estado + '\'';
+    }
 
 }
 
@@ -92,4 +85,3 @@ public String toString() {
     
     throw new UnsupportedOperationException("Unimplemented method 'setIdIncidencia'");
 } */
-
